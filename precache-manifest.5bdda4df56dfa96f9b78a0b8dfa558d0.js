ab992ccbafb89e727f8d350dcd187fff73385aaa
Mohit <mohit.patil@terafinainc.com>
2019-02-25 11:31:05 -0800

    Updates

---
+++ precache-manifest.5bdda4df56dfa96f9b78a0b8dfa558d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "099e9c5b46b3381791f64554b5e3ff2d",
+    "url": "/getYourlyrics/static/media/bounce.099e9c5b.gif"
+  },
+  {
+    "revision": "bdaad17b478a5ae4cdf2",
+    "url": "/getYourlyrics/static/js/runtime~main.bdaad17b.js"
+  },
+  {
+    "revision": "989361e2949d4c7b94ee",
+    "url": "/getYourlyrics/static/js/main.989361e2.chunk.js"
+  },
+  {
+    "revision": "fb877757d091bd37cd2e",
+    "url": "/getYourlyrics/static/js/2.fb877757.chunk.js"
+  },
+  {
+    "revision": "989361e2949d4c7b94ee",
+    "url": "/getYourlyrics/static/css/main.e83a0a1c.chunk.css"
+  },
+  {
+    "revision": "fb877757d091bd37cd2e",
+    "url": "/getYourlyrics/static/css/2.63982207.chunk.css"
+  },
+  {
+    "revision": "14a381fcb8e6646fc8a1d0ce9cb00c11",
+    "url": "/getYourlyrics/index.html"
+  }
+];
